43e2ec356dbad11f0f26de37240e6c81ade413aa
Shaundra <shaundrahamilton5@gmail.com>
2024-04-20 13:58:34 -0400

    changed tab color

---
+++ Musem/NotificationPage.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationPage.swift
+//  Musem
+//
+//  Created by Shaundra Hamilton on 4/20/24.
+//
+
+import SwiftUI
+
+struct NotificationPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationPage()
+}
---
+++ Musem/SearchPage.swift
@@ -0,0 +1,18 @@
+//
+//  SearchPage.swift
+//  Musem
+//
+//  Created by Shaundra Hamilton on 4/20/24.
+//
+
+import SwiftUI
+
+struct SearchPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchPage()
+}
