c87e9c76f6611f3f43a222ab312b030bae97fe63
Shaundra <shaundrahamilton5@gmail.com>
2024-06-07 15:26:15 -0400

    added to sign in page and extras..

---
+++ Musem/sampleProfile.swift
@@ -0,0 +1,18 @@
+//
+//  sampleProfile.swift
+//  Musem
+//
+//  Created by Shaundra Hamilton on 6/6/24.
+//
+
+import SwiftUI
+
+struct sampleProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    sampleProfile()
+}
